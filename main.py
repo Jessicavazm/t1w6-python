82f82690e5241665b6537393407966699bb8aff5
Jessicavazm <jjessicavaz@gmail.com>
2024-06-09 11:11:58 +1000

    data types

--- main.py
+++ main.py
@@ -1,2 +1,38 @@
-print("Hello world!!")
-print(5 - 3)
+
+# print("Hello world!!")
+# print(5 - 3)
+
+# # print("It's a comment, it won't be printed")
+
+# #This is an interger 
+# print(9)
+
+# #This is a string
+# print("9")
+
+# #To find out the type of data, you can use (type)
+# print(type(9))
+
+# #Operators = - * / % 
+# print(3 - 1)
+# print(10 % 3)
+
+# #Boolean values
+# print(bool(2))
+# print(type(2))
+
+# #Comparison Operators <, >, <=, =>, ==, !=
+# print(3 < 2)
+# print(5 >= 5)
+# print("Hello".lower() == "hello") 
+print("Hello".upper() == "HELLO")
+print("Hello".upper())
+
+#Logical operators and, or, not
+print(True and False) # AND returns True when both operators are True
+print(False or True) # OR returns returns True when one or both operators are True
+print(not False) # NOT returns the opposite value
+
+
+
+
